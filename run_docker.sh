fcd8baa9672e9bf3ddfe37ef5ec550955dc28ed8
Samar Heydari <81321086+samarheydari@users.noreply.github.com>
2025-09-18 18:10:05 +0200

    force GPU

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,8 @@
+# Remove any existing container with the same name
+#docker rm -f explanation_tfa02 2>/dev/null
 # docker run -p <port_exposed_on_cloud_machine>:<PORT> \
 docker run -p 8080:8080 \
 --name explanation_tfa02 \
+--runtime=nvidia \
+--gpus all \
 explanation_tfa02
